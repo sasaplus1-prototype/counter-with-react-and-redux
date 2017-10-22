d8b5857a8025d3b29ca55f7dc02ebb2affdcfb45
sasaplus1 <sasaplus1@gmail.com>
2017-10-22 21:59:03 +0900

    improved

--- index.js
+++ index.js
@@ -12,22 +12,67 @@
    */
 
   function counter(state, action) {
-    // NOTE: or Redux.createStore(counter, 0);
-    if (typeof state === 'undefined') {
-      return 0;
-    }
-
     switch (action.type) {
       case 'DECREMENT':
-        return state - 1;
+        return Object.assign({}, state, {
+          count: state.count - 1
+        });
       case 'INCREMENT':
-        return state + 1;
+        return Object.assign({}, state, {
+          count: state.count + 1
+        });
       default:
         return state;
     }
   }
 
-  const store = Redux.createStore(counter);
+  const store = Redux.createStore(counter, {
+    count: 0
+  });
+
+  //----------------------------------------------------------------------------
+
+  function mapStateToProps(state) {
+    return Object.assign({}, state);
+  }
+
+  function mapDispatchToProps(dispatch) {
+    return {
+      decrement() {
+        dispatch({
+          type: 'DECREMENT'
+        });
+      },
+      increment() {
+        dispatch({
+          type: 'INCREMENT'
+        });
+      }
+    };
+  }
+
+  const Counter = ReactRedux.connect(mapStateToProps, mapDispatchToProps)(class extends React.Component {
+    render() {
+      const {
+        count,
+        decrement,
+        increment
+      } = this.props;
+
+      return React.createElement(
+        'div',
+        null,
+        React.createElement(
+          'span',
+          null,
+          count
+        ),
+        React.createElement('br', null),
+        React.createElement('input', { type: 'button', value: 'inc', onClick: increment }),
+        React.createElement('input', { type: 'button', value: 'dec', onClick: decrement })
+      );
+    }
+  });
 
   //----------------------------------------------------------------------------
 
@@ -38,26 +83,9 @@
       return React.createElement(
         Provider,
         { store: store },
-        React.createElement(
-          'div',
-          null,
-          React.createElement(
-            'span',
-            null,
-            1
-          ),
-          React.createElement('br', null),
-          React.createElement('input', { type: 'button', value: 'inc' }),
-          React.createElement('input', { type: 'button', value: 'dec' })
-        )
+        React.createElement(Counter, null)
       );
     }
-  }
-
-  function mapStateToProps(state) {
-    return {
-      state: state
-    };
   }
 
   //----------------------------------------------------------------------------
--- src/index.js
+++ src/index.js
@@ -11,22 +11,65 @@
    * @param {Object} action
    */
   function counter(state, action) {
-    // NOTE: or Redux.createStore(counter, 0);
-    if (typeof state === 'undefined') {
-      return 0;
-    }
-
     switch (action.type) {
       case 'DECREMENT':
-        return state - 1;
+        return Object.assign({}, state, {
+          count: state.count - 1,
+        });
       case 'INCREMENT':
-        return state + 1;
+        return Object.assign({}, state, {
+          count: state.count + 1,
+        });
       default:
         return state;
     }
   }
 
-  const store = Redux.createStore(counter);
+  const store = Redux.createStore(counter, {
+    count: 0,
+  });
+
+  //----------------------------------------------------------------------------
+
+  function mapStateToProps(state) {
+    return Object.assign({}, state);
+  }
+
+  function mapDispatchToProps(dispatch) {
+    return {
+      decrement() {
+        dispatch({
+          type: 'DECREMENT',
+        });
+      },
+      increment() {
+        dispatch({
+          type: 'INCREMENT',
+        });
+      },
+    };
+  }
+
+  const Counter = ReactRedux.connect(mapStateToProps, mapDispatchToProps)(
+    class extends React.Component {
+      render() {
+        const {
+          count,
+          decrement,
+          increment,
+        } = this.props;
+
+        return (
+          <div>
+            <span>{count}</span>
+            <br />
+            <input type="button" value="inc" onClick={increment} />
+            <input type="button" value="dec" onClick={decrement} />
+          </div>
+        );
+      }
+    }
+  );
 
   //----------------------------------------------------------------------------
 
@@ -36,21 +79,10 @@
     render() {
       return (
         <Provider store={store}>
-          <div>
-            <span>{1}</span>
-            <br />
-            <input type="button" value="inc" />
-            <input type="button" value="dec" />
-          </div>
+          <Counter />
         </Provider>
       );
     }
-  }
-
-  function mapStateToProps(state) {
-    return {
-      state: state
-    };
   }
 
   //----------------------------------------------------------------------------
